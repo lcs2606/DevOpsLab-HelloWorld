308da9e3b436b9530eb799cabe34d638fe95bbba
Luis Carlos <luis.carlos.1995@hotmail.com>
2021-10-05 14:36:11 -0400

    correção do codigo

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline Devops - Luiss Carlos")
+        self.assertRegex(result.data.decode(), "Pipeline Devops - Luis Carlos")
 
 
 if __name__ == "__main__":
